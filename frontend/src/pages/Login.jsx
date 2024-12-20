395b56b6334d28eaa8d5393255d79694e21db3e5
nariman <bazarbay.nariman@gmail.com>
2024-12-21 01:56:51 +0500

    fix: heigh form in login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,10 @@ import { useNavigate, Link } from "react-router-dom";
 import "../styles/Login.css";
 
 const Login = () => {
-  const [credentials, setCredentials] = useState({ username: "", password: "" });
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { status, error } = useSelector((state) => state.user);
@@ -23,41 +26,43 @@ const Login = () => {
   };
 
   return (
-    <div className="form-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter your username"
-            value={credentials.username}
-            onChange={handleChange}
-            required
-          />
+    <div className="form-wrapper">
+      <div className="form-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+              value={credentials.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              value={credentials.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit" disabled={status === "loading"}>
+            Login
+          </button>
+        </form>
+        {status === "loading" && <p>Loading...</p>}
+        {status === "failed" && (
+          <p style={{ color: "red" }}>
+            {error?.detail || "Invalid credentials, please try again!"}
+          </p>
+        )}
+        <div className="forgot-password">
+          <Link to="/password-reset">Forgot Password?</Link>
         </div>
-        <div>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={credentials.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" disabled={status === "loading"}>
-          Login
-        </button>
-      </form>
-      {status === "loading" && <p>Loading...</p>}
-      {status === "failed" && (
-        <p style={{ color: "red" }}>
-          {error?.detail || "Invalid credentials, please try again!"}
-        </p>
-      )}
-       <div className="forgot-password">
-        <Link to="/password-reset">Forgot Password?</Link>
       </div>
     </div>
   );
